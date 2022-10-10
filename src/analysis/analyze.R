01b9c1bac25a207879681fdae33ff9cdfa7d9a8b
Bo de Ruijter <bodr@ww140940.uvt.nl>
2022-10-10 13:34:54 +0200

    analysis change

--- src/analysis/analyze.R
+++ src/analysis/analyze.R
@@ -1,11 +1,19 @@
-# load 
-load("./gen/analysis/input/data_cleaned.RData")
+# Read cleaned dateset 
+cleaned_dataset <- read.csv("cleaned_dataset.csv")
 
-# Estimate model 1 
+summarize(cleaned_dataset)
+
+table(cleaned_dataset$room_type)
+
+# Estimate model 1 --> Dit stond in de template
 m1 <- lm(V1 ~ V3 + V4,df_cleaned)
 
-# Estimate model 2 
+# Estimate model 2--> Dit stond in de template
 m2 <- lm(V1 ~ V3 + V4 + V5 , df_cleaned)
+
+# Wij dachten wellicht meerdere ANOVA's runnen om significante verschillen te indiceren. 
+
+# Onderlinge verschillen tussen steden, tussen europa en US en evt. verschil tussen de verschillen (e.g., in europa verschilt het zoveel... en us verschilt het zoveel.. is het verschil tussen die 2 verschillen een significant verschil)
 
 # Save results
 save(m1,m2,file="./gen/analysis/output/model_results.RData")
