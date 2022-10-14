eb44d2b35f395ebdfefd1fd6c58aa9b1f4ecfebd
Pepijn <pepijndevries@casema.nl>
2022-10-14 12:16:06 +0200

    Implemented the code of analysis into cleaning file

--- src/data-preparation/cleaning_file.r
+++ src/data-preparation/cleaning_file.r
@@ -6,23 +6,47 @@ listing_data <- read.csv("listing_data.csv")
 merged_data <- calender_data %>%
   left_join(listing_data, by = c("listing_id" = "id"))
 
-merged_data_without_na <- na.omit(merged_data)
+cleaned_dataset <- na.omit(merged_data)
 
-merged_data_without_na$date <- as.Date(merged_data_without_na$date)
+cleaned_dataset$date <- as.Date(cleaned_dataset$date)
 
-merged_data_without_na$day_num <- format(merged_data_without_na$date,"%u")
-merged_data_without_na$day_num <- as.numeric(merged_data_without_na$day_num)
+cleaned_dataset$day_num <- format(cleaned_dataset$date,"%u")
+cleaned_dataset$day_num <- as.numeric(cleaned_dataset$day_num)
 
-merged_data_without_na$weekdag <- weekdays(merged_data_without_na$date)
+cleaned_dataset$weekdag <- weekdays(cleaned_dataset$date)
 
   
 weekdays1 <- c(1, 2, 3, 4, 7)
-merged_data_without_na$wDay <- factor(((merged_data_without_na$day_num) %in% weekdays1), levels=c(FALSE, TRUE), labels=c('weekend', 'weekday'))
+cleaned_dataset$wDay <- factor(((cleaned_dataset$day_num) %in% weekdays1), levels=c(FALSE, TRUE), labels=c('weekend', 'weekday'))
 
 
 # We still need to change the price variable into a variable that stores the numeric value of price without the dollar sign
-merged_data_without_na$price <- parse_number(merged_data_without_na$price)
+cleaned_dataset$price <- parse_number(cleaned_dataset$price)
 
-write.csv(merged_data_without_na, "cleaned_dataset.csv")
+# Create dataset for analyses 
+## Delete columns that are not needed for analyses
+cleaned_dataset <- cleaned_dataset %>% select(-adjusted_price)
+cleaned_dataset <- cleaned_dataset %>% select(-maximum_nights)
+cleaned_dataset <- cleaned_dataset %>% select(-X.y)
+cleaned_dataset <- cleaned_dataset %>% select(-listing_id)
+cleaned_dataset <- cleaned_dataset %>% select(-available)
+cleaned_dataset <- cleaned_dataset %>% select(-X.x)
+cleaned_dataset <- cleaned_dataset %>% select(-date)
+
+## Make dummy variable of variable wDay 
+cleaned_dataset$wDay <- ifelse(cleaned_dataset$wDay == "weekday", 1,0)
+
+## Create new column for short-term stays
+short_term_stays <- c('1', '2', '3', '4')
+cleaned_dataset$short_term_stays <- factor(cleaned_dataset$minimum_nights %in% short_term_stays)
+
+## Create new column for cities in U.S. and Europe
+united_states <- c('denver', 'portland', 'san-francisco', 'los-angeles', 'new-york-city')
+cleaned_dataset$united_states <- factor(cleaned_dataset$city %in% united_states)
+
+europe <- c('munich', 'london', 'paris', 'milan', 'dublin')
+cleaned_dataset$europe <-factor(cleaned_dataset$city %in% europe)
+
+write.csv(cleaned_dataset, "cleaned_dataset.csv")
 
 # File copy needs to be copied towards the gen/analysis/input
